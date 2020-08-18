606cc50c20109532c5cc266a2e9c6a5bb9da2605
lolwhenlifegivesyoulemons <62519659+lolwhenlifegivesyoulemons@users.noreply.github.com>
2020-08-18 13:32:22 -0600

    (click me!!) fixed playlists with tvg's and groups + iptv-org eng playlist
    please report issues!!!!, some playlists might show undefined or not show at all

--- js/parser.js
+++ js/parser.js
@@ -4,6 +4,22 @@ var Parser = (function() {
   return {
     // Very simple m3u parser
     parse: function(data) {
+      if (data.includes('tvg')) {
+        //i hate tvgs and group stuff :(
+        data = data.split(' ');
+        data.forEach((elem, index)=>{
+          if(elem.includes('tvg-')) data.splice(index, 1);
+        });
+        data.forEach((elem, index)=>{
+          if(elem.includes('tvg-logo')) data.splice(index, 1);
+        });
+        data.forEach((elem, index)=>{
+          if(!elem.split('"')[1]) return;
+          if(elem.includes('24/7')) data[index] = "" //patch out undefined if channel includes 24/7
+          if(elem.startsWith('group')) data[index] = ","+elem.split('"')[1]+" ~ "+elem.split('"')[2].replace(',', '');
+        })
+        data = data.join(' ').replace(/1 ,/g, '1,');
+      }
       var lastName;
       var channels = {};
       data = data.split('\r\n');
